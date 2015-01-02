1552def66ea02ca660803a7ae2042feb96cf767b
DawnBai <sidong.bai@gmail.com>
2015-01-02 23:39:27 +0800

    wrap read and write function

--- wrap.c
+++ wrap.c
@@ -78,3 +78,109 @@ void Close(int fd)
 	if (-1 == close(fd))
 		perr_exit("close error");
 }
+
+ssize_t Readn(int fd, void *vptr, size_t n)
+{
+	size_t nleft;
+	ssize_t nread;
+	char *ptr;
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0)
+	{
+		if ( (nread = read(fd, ptr, nleft)) < 0)
+		{
+			if (errno == EINTR)
+				nread = 0;
+			else
+				return -1;
+		} else if ( 0 == nread )
+			break;
+
+		nleft -= nread;
+		ptr += nread;
+	}
+	return n - nleft;
+}
+
+ssize_t Writen (int fd, const void *vptr, size_t n)
+{
+	size_t nleft;
+	ssize_t nwritten;
+	const char *ptr;
+	
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0)
+	{
+		if ((nwritten = write(fd, ptr, nleft)) <= 0)
+		{
+			if (nwritten < 0 && errno == EINTR)
+				nwritten = 0;
+			else
+				return -1;
+		}
+		
+		nleft -= nwritten;
+		ptr += nwritten;
+	}
+	return n;
+}
+
+static ssize_t my_read(int fd, char *ptr)
+{
+	static int read_cnt = 0;
+	static char *read_ptr = NULL;
+	static char read_buf[100];
+
+	if (read_cnt <= 0)
+	{
+	again:
+		if ( (read_cnt = read(fd, read_buf, sizeof(read_buf)))<0 )
+		{
+			if (EINTR == errno)
+			{
+				goto again;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+		else if (0 == read_cnt)
+			return 0;
+		
+		read_ptr = read_buf;
+	}
+	read_cnt--;
+	*ptr = *read_ptr++;
+	return 1;
+}
+
+ssize_t Readline(int fd, void *vptr, size_t maxlen)
+{
+	ssize_t n, rc;
+	char c, *ptr;
+	for (n=1; n<maxlen; n++)
+	{
+		if ((rc= my_read(fd, &c)) == 1)
+		{
+			*ptr++ = c;
+			if ( '\n' == c )
+			{
+				break;
+			}
+		}
+		else if (0 == rc)
+		{
+			*ptr = 0;
+			return n - 1;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	*ptr = 0;
+	return n;
+}
